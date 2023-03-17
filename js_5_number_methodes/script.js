373b8c9fcf018d15cd3adbdbd0c640f38fa4ba73
Harry Fenitra <harryfenitraherredias@gmail.com>
2023-03-17 04:37:40 +0300

    pushiing the folders in my repository

---
+++ js_5_number_methodes/script.js
@@ -0,0 +1,6 @@
+var repeat_number = document.getElementsByClassName("nbr_repeat");
+var x = 123;
+
+      for ( var i = 0; i < repeat_number.length ; i++){
+          repeat_number[i].innerHTML = x;
+         }
